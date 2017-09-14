8df1d18e89d621462ac80a1c4f2da4bdee1d8a50
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-13 17:05:16 -0700

    CatGarden works. Woot!

--- catgarden/catgarden/Program.cs
+++ catgarden/catgarden/Program.cs
@@ -6,7 +6,28 @@ namespace catgarden
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Array[] garden = new Array[5];
+
+            for (int i = 0; i < garden.Length; i++)
+            {
+                if ((i + 1) % 2 != 0)
+                {
+                    string[] rowCatNip = new string[] {"catnip", "catnip", "catnip", "catnip", "catnip",};
+                    garden[i] = rowCatNip;
+                }
+                else
+                {
+                    string[] rowMint = new string[] { "mint", "mint", "mint", "mint", "mint", };
+                    garden[i] = rowMint;
+                }
+            }
+            foreach (var item in garden)
+            {
+                foreach (var stuff in item)
+                {
+                    Console.WriteLine(stuff);
+                }
+            }
         }
     }
 }
