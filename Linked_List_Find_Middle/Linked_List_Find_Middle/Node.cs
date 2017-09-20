09a3b9ffe29e78fabc0bb32c3ee57cf2bd40a1f4
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-19 18:05:02 -0700

    Finished

---
+++ Linked_List_Find_Middle/Linked_List_Find_Middle/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Linked_List_Find_Middle
+{
+    class Node
+    {
+        public Node Next { get; set; }
+        public Object Data { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ Linked_List_Find_Middle/Linked_List_Find_Middle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Linked_List_Find_Middle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList myList = new LinkedList(24);
+            myList.Add(13);
+            myList.Add(15);
+            myList.Add(9);
+            myList.Add(8);
+            myList.PrintMiddle();
+            Console.WriteLine(myList.midCount);
+        }
+    }
+}
