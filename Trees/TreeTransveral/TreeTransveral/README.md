f1847082ce7ffba4c2bc747c5a2fddd5e8973f47
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-03 20:58:15 -0700

    Adding Readme

---
+++ Trees/TreeTransveral/TreeTransveral/README.md
@@ -0,0 +1,4 @@
+﻿This took a bit of work. Maybe 2 hours.
+Struggled with the logic to move up and down the tree
+Learned how to store a string using ref
+Stack overflow
---
+++ Trees/TreeTransveral/TreeTransveral/Tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TreeTransveral
+{
+    class Tree
+    {
+        public Leaf root;
+
+        public Tree(Leaf data)
+        {
+            root = data;
+        }
+
+        public void PrintTree(Leaf L, ref string s)
+        {
+            if (L.Left != null)
+            {
+                PrintTree(L.Left, ref s);
+                s = s + L.Value.ToString().PadLeft(3);
+            }
+            else
+            {
+                s = s + L.Value.ToString().PadLeft(3); 
+            }
+            if (L.Right != null)
+            {
+                PrintTree(L.Right, ref s);
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
