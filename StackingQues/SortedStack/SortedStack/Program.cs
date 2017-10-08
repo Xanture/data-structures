b7c408b1ff3d2568d19a5b2f93eafef7a0821e5c
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-07 22:07:07 -0700

    Created Push Pop and View methods for Stack and tested them.

--- StackingQues/SortedStack/SortedStack/Program.cs
+++ StackingQues/SortedStack/SortedStack/Program.cs
@@ -6,6 +6,15 @@ namespace SortedStack
     {
         static void Main(string[] args)
         {
+            Stack stack = new Stack(2);
+            stack.Push(3);
+            stack.Push(5);
+            stack.Push(1);
+            stack.Push(4);
+            stack.Push(6);
+
+            stack.View();
+
         }
     }
 }
---
+++ StackingQues/SortedStack/SortedStack/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedStack
+{
+    class Stack
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        //Constructor for when I create a stack I want it created with a Node and set that Node to head
+        public Stack(int data)
+        {
+            Head = new Node(data);
+            Tail = Head;
+        }
+
+        //Push is setting a new head on top of the stack and creating a reference to the next below
+        public void Push(int data)
+        {
+            Node temp = Head;
+            Head = new Node(data);
+            Head.Next = temp;
+        }
+
+        //Pop is removing the reference to the top node and returning it
+        public int Pop()
+        {
+            if (Head == Tail)
+            {
+                Tail = null;
+            }
+            Node temp = Head.Next;
+            Head.Next = null;
+            int value = Head.Data;
+            Head = temp;
+            return value;
+        }
+
+        public void View()
+        {
+            Node curr = Head;
+            while (curr != null)
+            {
+                Console.WriteLine(curr.Data);
+                curr = curr.Next;
+            }
+
+        }
+    }
+}
