2d0fac2d8355f82fa0367479d173407a7685dc23
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-08 19:00:23 -0700

    Created Node Class

---
+++ Linked_Lists/DLinkedLIst/DLinkedLIst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DLinkedLIst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
